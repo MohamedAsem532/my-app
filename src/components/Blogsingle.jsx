a382b7689656d081ec0fbce6f1debc87f84027a3
Mohamed Asem <105158007+MohamedAsem532@users.noreply.github.com>
2025-10-18 02:36:00 +0300

    feat: enhance Blogsingle component with hover effects and improved styling

--- src/components/Blogsingle.jsx
+++ src/components/Blogsingle.jsx
@@ -1,9 +1,10 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import './Headerc.css'
+import Hcomponent from './Hcomponent';
 function Blogsingle(props) {
   return (
-    <Card >
+    <Card className='shadow p-3 mb-5 bg-body rounded striped bordered hover card-hover' >
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
